65c3a0775ec2bd4dc5ed72f762e816ebba691607
Vincent <vincentseitlhamo@gmail.com>
2024-09-05 14:10:12 +0200

    initial

---
+++ authenticate/src/main/java/za/co/projects/AuthenticateApplication.java
@@ -0,0 +1,13 @@
+package za.co.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateApplication.class, args);
+	}
+
+}
---
+++ authenticate/src/test/java/za/co/projects/AuthenticateApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
